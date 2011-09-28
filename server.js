70c8970fc6c36ad996b00bf830f24bfb87cf6ec7
ben <bmills@benmillsdesigns.com>
2011-09-27 19:27:50 -0500

    Added redirect server

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var http = require('http');
-var fs   = require('fs');
-var url  = require('url');
 
 http.createServer(function (req, res) {
-  var reqFile = url.parse(req.url).pathname;
-  if (reqFile === "/") reqFile = "/index.html";
-
-  fs.readFile(reqFile.substr(1), function (err, data) {
-    if (err) res.end();
-    else res.end(data.toString());
+  res.writeHead(302, {
+    'Location': 'http://benmills.org'
   });
+  res.end();
 }).listen(process.env.PORT || 8080);
